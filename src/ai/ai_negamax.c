d8e6b999112f6649b1fc040db870165c55d79787
GawinGowin <saraki@student.42tokyo.jp>
2026-05-17 15:59:50 +0900

    update: time based fallback

--- src/ai/ai_negamax.c
+++ src/ai/ai_negamax.c
@@ -1,6 +1,7 @@
 #include "connect4.h"
 
 #include <limits.h>
+#include <time.h>
 
 #define WIN_SCORE 100000000
 
@@ -8,30 +9,42 @@ static int
 negamax(t_board *b, int depth, int alpha, int beta, t_cell player_mark, const int *order);
 
 int ai_negamax(t_board *b, t_cell player_mark) {
-  t_cell opponent_mark = OPPONENT(player_mark);
   int remaining = b->rows * b->cols - b->moves_count;
-  int depth = MIN(6, remaining);
-  int best_score = INT_MIN;
-  int best_col = -1;
+
   int order[MAX_COLS];
 
   build_order_array(order, b->cols, MAX_COLS);
-  for (int i = 0; i < b->cols; i++) {
-    int col = order[i];
-    if (b->stack_top[col] >= b->rows)
-      continue;
-    int last_row = b->stack_top[col];
-    board_drop(b, col, player_mark);
-    if (board_check_win(b, last_row, col)) {
+  int best_col = order[0];
+
+  time_t start = time(NULL);
+
+  for (int depth = 1; depth <= remaining; depth++) {
+    if (time(NULL) - start >= 1) {
+      break;
+    }
+    int best_score = INT_MIN + 1; // '-best_score' overflow care
+    int depth_best = best_col;
+
+    for (int i = 0; i < b->cols; i++) {
+      int col = order[i];
+      if (b->stack_top[col] >= b->rows)
+        continue;
+      int last_row = b->stack_top[col];
+      board_drop(b, col, player_mark);
+      if (board_check_win(b, last_row, col)) {
+        board_undo(b, col);
+        return col;
+      }
+      int score = -negamax(b, depth - 1, INT_MIN + 1, INT_MAX, OPPONENT(player_mark), order);
       board_undo(b, col);
-      return col;
+      if (score > best_score) {
+        best_score = score;
+        depth_best = col;
+      }
     }
-    int score = -negamax(b, depth - 1, INT_MIN + 1, INT_MAX, opponent_mark, order);
-    board_undo(b, col);
-    if (score > best_score) {
-      best_score = score;
-      best_col = col;
-    }
+    best_col = depth_best;
+    if (best_score >= WIN_SCORE - depth)
+      break;
   }
   return best_col;
 }
