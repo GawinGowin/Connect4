a017d4ab74b0828129b5c89fda597ba4dc5bd722
Shogo Araki <101625248+GawinGowin@users.noreply.github.com>
2026-05-17 16:48:19 +0900

    update: replace ai algo functions

--- src/ai/ai.c
+++ src/ai/ai.c
@@ -1,8 +1,3 @@
 #include "connect4.h"
 
-int ai_choose_move(t_board *board, t_cell player_mark) {
-  if (board->cols < 8 && board->rows < 8) {
-    return ai_negamax(board, player_mark);
-  }
-  return ai_greedy(board, player_mark);
-}
+int ai_choose_move(t_board *board, t_cell player_mark) { return ai_negamax(board, player_mark); }
